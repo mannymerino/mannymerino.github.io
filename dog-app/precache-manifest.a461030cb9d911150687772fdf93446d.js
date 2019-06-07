30e5bcd3c813b9d0cd61195f0cebaeeb7020aa91
manny <manny@legworkprm.com>
2019-06-07 09:10:41 -0700

    dog-app bundle added

---
+++ dog-app/precache-manifest.a461030cb9d911150687772fdf93446d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90aa53bae1c2fbefab61562ae895078e",
+    "url": "/dog-app/index.html"
+  },
+  {
+    "revision": "6df8bd039ebde8b1fa0a",
+    "url": "/dog-app/static/css/main.7a35f76d.chunk.css"
+  },
+  {
+    "revision": "6c6bd9116f13c83ffc01",
+    "url": "/dog-app/static/js/2.116b7e44.chunk.js"
+  },
+  {
+    "revision": "6df8bd039ebde8b1fa0a",
+    "url": "/dog-app/static/js/main.eb5f178d.chunk.js"
+  },
+  {
+    "revision": "13f41eb9af751c6a0405",
+    "url": "/dog-app/static/js/runtime~main.f1bd9a0b.js"
+  }
+]);
